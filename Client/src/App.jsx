8348e81b1fa6615c8052db7eb654daeb213627de
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-02 13:09:14 -0300

    cambio

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Portfolio</h1>
       <div className="card">
         <div>
           {count}
