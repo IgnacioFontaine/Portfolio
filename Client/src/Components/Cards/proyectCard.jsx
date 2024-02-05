5dfe5a544643567f0f750add367cd5f50728dfd5
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-05 10:11:08 -0300

    proyect card

--- Client/src/Components/Cards/proyectCard.jsx
+++ Client/src/Components/Cards/proyectCard.jsx
@@ -1,2 +1,14 @@
-//Hola
-{"Soy el Proyect Card"}
+const ProyectCard = () => {
+   return (
+     <div>
+       <div>
+         <h1>Proyect Card</h1>
+       </div>
+       <div>
+         <h2>Parametros</h2>
+       </div>
+     </div>
+  );
+};
+
+export default ProyectCard;
