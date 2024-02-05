d21d6dccb0e29e230ce8648ce269d241e60a0374
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-05 10:06:18 -0300

    proyects en proceso

--- Client/src/Views/Proyects/proyects.jsx
+++ Client/src/Views/Proyects/proyects.jsx
@@ -1,20 +1,14 @@
 
-
 const Proyects = () => {
-  const navigate = useNavigate()
    return (
-     <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", bgcolor:"#D5CECE", color:"black", height:"500px", width:"400px", borderRadius:4, alignContent:"center"}}>
-       <Box sx={{ mt: 5, alignItems:"center", alignContent:"center", justifyItems:"center"  }}>
-         <Icon />
-         <Typography variant="h2" sx={{fontFamily:"Hamston", textDecoration:"bold", mt:1}} >WERISCO</Typography>
-       </Box>
-       <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", gap:1, m:3}}>
-         <Button sx={{textDecoration:"none", color:"black"}} onClick={()=>navigate("/stock")}>Stock</Button>
-         <Button sx={{textDecoration:"none", color:"black"}} onClick={() => navigate("/sold")}>Ventas</Button>
-         <Button sx={{textDecoration:"none", color:"black"}} onClick={() => navigate("/companies")}>Companias</Button>
-         <Button sx={{textDecoration:"none", color:"black"}} onClick={()=>navigate("/metrics")}>Estadísticas</Button>
-       </Box>
-    </Box>
+     <div>
+       <div>
+         <h1>Proyects</h1>
+       </div>
+       <div>
+         <h2>List proyects</h2>
+       </div>
+     </div>
   );
 };
 
