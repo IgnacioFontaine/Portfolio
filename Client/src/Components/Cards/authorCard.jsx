b6f598246d1bc523464aa8473d48174a3039b696
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-05 10:09:29 -0300

    author card en proceso

--- Client/src/Components/Cards/authorCard.jsx
+++ Client/src/Components/Cards/authorCard.jsx
@@ -1,2 +1,14 @@
-//Hola
-{"Soy el Author card"}
+const Proyects = () => {
+   return (
+     <div>
+       <div>
+         <h1>Proyects</h1>
+       </div>
+       <div>
+         <h2>List proyects</h2>
+       </div>
+     </div>
+  );
+};
+
+export default Proyects;
