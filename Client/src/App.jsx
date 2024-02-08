d8fca7e6f85ae7ac15af4596db1313af4a581e7b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-08 10:47:17 -0300

    Paginas en app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
+import Home from "./Views/Home/home"
+import Error from "./Views/Error/error"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />}  /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />}  />
       </Routes>
     </div>
   )
