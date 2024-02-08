6ea886745418bee04e062c73abbdbbb81591161b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-08 11:22:16 -0300

    cambios pero no se visualiza el home

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -1,7 +1,6 @@
-import { applyMiddleware } from "redux";
-import { legacy_createStore as createStore} from 'redux'
+import { createStore, applyMiddleware } from "redux"; // Importa createStore directamente
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
 import reducer from "./reducer";
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
