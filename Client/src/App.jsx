fa8c6872e16580ba9d3f2a5c2360b9fd3c03f35b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-08 10:46:09 -0300

    app en proceso

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,38 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Portfolio</h1>
-      <div className="card">
-        <div>
-          {count}
-        </div>
-        <div>
-          <button onClick={() => setCount((count) => count + 1)}>
-          +
-        </button>
-        </div>
-        <div>
-          <button onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />}  /> */}
+      </Routes>
+    </div>
   )
 }
 
