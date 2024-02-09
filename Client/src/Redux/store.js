f40e7044dab53dd2365b9b707ce8f544540578c0
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-09 10:21:42 -0300

    thunk declarado bien

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux"; // Importa createStore directamente
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from 'redux-thunk';
 import reducer from "./reducer";
+import { thunk } from "redux-thunk";
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
