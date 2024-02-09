faf95461952c53ed005c373006d6a87183d0f483
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-09 10:25:16 -0300

    error view

--- Client/src/Views/Error/error.jsx
+++ Client/src/Views/Error/error.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Soy el error</h1>
+      <div>
+        <button onClick={()=>{navigate("/")}}>Return to home</button>
+      </div>
     </div>
   );
 };
