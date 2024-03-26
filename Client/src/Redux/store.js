1c43fee48c08b3a0f67144492932e05ea633856e
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-26 12:10:18 -0300

    redux eliminado

--- Client/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux"; // Importa createStore directamente
-import { composeWithDevTools } from "redux-devtools-extension";
-import reducer from "./reducer";
-import { thunk } from "redux-thunk";
-
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
