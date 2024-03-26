fc44e06193544c82c7c207bc87263ad540b64ee0
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-26 12:52:36 -0300

    cambio en ed experience

--- Client/src/Components/EdExperience/edExperience.jsx
+++ Client/src/Components/EdExperience/edExperience.jsx
@@ -1,4 +1,22 @@
+
+
 const EdExperience = () => {
+  const edExperience =
+    [{
+      id: 1,
+      title: "Full Stack",
+      description: "Botcamp de SoyHenry, con aprendizajes teóoricos y prácticos en cada área de la programación Web, tanto Front End como Back End y Bases de Datos."
+    },
+    {
+      id: 2,
+      title: "Pasantía WhataCart",
+      description: "Pasantía con una duración de 3 meses para la empresa WhataCart en la cual junto con un equipo realizamos una Web con el fin de trabajar  via Whatsap haciendo marketing digital, implementamos para ello una API"
+    },
+    {
+      id: 3,
+      title: "Idiomas",
+      description: "Test EFSET que nivela el nivel en el idioma inglés, la clasificación obtenida al realizar el test fue de: B1"
+      }]
   return (
     <div>
     </div>
