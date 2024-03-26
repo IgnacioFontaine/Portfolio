ef042ef94b3d93b89213d5985efae38b5c5c1bb1
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-26 12:59:57 -0300

    cambio en proyects

--- Client/src/Components/LabExperience/labExperience.jsx
+++ Client/src/Components/LabExperience/labExperience.jsx
@@ -1,7 +1,13 @@
 const LabExperience = () => {
+  const labExperience =
+    [ { id: 4, name: "Werisco", date: "" },
+      { id: 5, name: "Calzados Livon", date: "" },
+      { id: 6, name: "Alto Tejeda", date: "" },
+      { id: 7, name: "Alojando", date: "" }]
+  
   return (
-    <div>
-    </div>
+    <section>
+    </section>
   );
 };
 
--- Client/src/Components/Proyects/proyects.jsx
+++ Client/src/Components/Proyects/proyects.jsx
@@ -1,4 +1,11 @@
 const Proyects = () => {
+  const all_proyects =
+    [ { id: 8, name: "WhataCart", description: "", proyect_image: "", tecnologies: [""] },
+      { id: 9, name: "FonteShop", description: "", proyect_image: "", tecnologies: [""] },
+      { id: 10, name: "Clima Argentino", description: "", proyect_image: "", tecnologies: [""] },
+      { id: 11, name: "VideoGames", description: "", proyect_image: "", tecnologies: [""] },
+      { id: 12, name: "Rick&Morty", description: "", proyect_image: "", tecnologies: [""] },]
+  
    return (
      <div>
      </div>
